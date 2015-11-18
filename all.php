5e0ffb7607538642cb3bbeb230357ac8479ef4b7
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-18 22:51:13 +0700

    Update all.php

--- all.php
+++ all.php
@@ -1,7 +1,20 @@
 <html>
 	<body>
+		<?php
+			include("config.php");
+		?>
 			<tr align="left">
 				<td width="376">
+					<?php
+						$query=mysql_query("select * from posted");
+
+						while($data=mysql_fetch_array($query)){
+							echo $data['title'];
+							echo $data['isi'];
+						}
+						
+						mysql_close();
+					?>
 				</td>
 				<td width="376">
 				</td>

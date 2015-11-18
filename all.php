14f83e1659ba4371599e7e6a8f89c8c8db5fefa5
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-19 01:02:28 +0700

    Update all.php

--- all.php
+++ all.php
@@ -9,7 +9,7 @@
 						$query=mysql_query("select * from posted where id=1");
 
 						while($data=mysql_fetch_array($query)){
-							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
+							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
 						}
@@ -20,7 +20,7 @@
 						$query=mysql_query("select * from posted where id=2");
 
 						while($data=mysql_fetch_array($query)){
-							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
+							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
 						}
@@ -31,7 +31,7 @@
 						$query=mysql_query("select * from posted where id=3");
 
 						while($data=mysql_fetch_array($query)){
-							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
+							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
 						}
@@ -44,7 +44,7 @@
 						$query=mysql_query("select * from posted where id=4");
 
 						while($data=mysql_fetch_array($query)){
-							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
+							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
 						}
@@ -55,7 +55,7 @@
 						$query=mysql_query("select * from posted where id=5");
 
 						while($data=mysql_fetch_array($query)){
-							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
+							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
 						}
@@ -66,7 +66,7 @@
 						$query=mysql_query("select * from posted where id=6");
 
 						while($data=mysql_fetch_array($query)){
-							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
+							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
 						}

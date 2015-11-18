1297faedfedbe3d6ebd950f62447cc2cf35a0cd5
fyucee <yudafyuce@yahoo.co.id>
2015-11-18 22:01:18 +0700

    First initial

---
+++ all.php
@@ -0,0 +1,22 @@
+<html>
+	<body>
+			<tr align="left">
+				<td width="376">
+				</td>
+				<td width="376">
+				</td>
+				<td width="376">
+				</td>
+			</tr>
+			<tr align="left">
+				<td>
+				</td>
+				<td>
+				</td>
+				<td>
+				</td>
+			</tr>
+		</table>
+
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+		<title>Cogame</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<table align="center" id="header" height="100" width="1166">
+			<tr>
+				<td width="5%" align="center">
+					<a href="index.php"><img id="title" src="image/logo.png" width="100" height="100"></a>
+				</td>
+				<td >
+					<a id="title" href="index.php"><font size="7">Conio Gaming</font></a>
+				</td>
+			</tr>
+		</table>
+
+		<table align="center" id="menu" width="1130">
+			<tr align="center" height="50">
+				<td align="left">
+					<a href="index.php?category=all"><img id="menu" src="image/menu_all.png"></a>
+					<a href="index.php?category=pc"><img id="menu" src="image/menu_pc.png"></a>
+					<a href="index.php?category=ps4"><img id="menu" src="image/menu_ps4.png"></a>
+					<a href="index.php?category=xboxone"><img id="menu" src="image/menu_xboxone.png"></a>
+					<a href="index.php?category=wiiu"><img id="menu" src="image/menu_wiiu.png"></a>
+					<a href="index.php?category=psvita"><img id="menu" src="image/menu_psvita.png"></a>
+					<a href="index.php?category=mobile"><img id="menu" src="image/menu_mobile.png"></a>
+				</td>
+				<td id="search" width="170">
+						<input style="border:none; width:100" type="text" placeholder="Search">
+						<input type="image" src="image/search.png" style="vertical-align: middle;" height="20px" width="20px"></a>
+				</td>
+			</tr>
+		</table>
+
+		<table  align="center" id="ads" width="1130">
+			<tr align="center" height="100">
+				<td>Advertise</td>
+			</tr>
+		</table>
+		
+		<table  align="center" id="posted" width="1130">
+			<?php
+					if(!isset($_GET['category'])){
+						include('all.php');
+					}
+					else{
+						$page = $_GET['category'].".php";
+						include($page);
+					}
+				?>
+		</table>
+
+		<table  align="center" id="copyright" height="50" width="1130">
+			<tr align="center">
+				<td height="50">Copyright by Cogame.com.<br>All Right Reserved.</td>
+			</tr>
+		</table>
+	</body>
+</html>

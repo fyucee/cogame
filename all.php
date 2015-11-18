65200e93d6b9e56558dbb8216218ecef67c3e733
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-18 23:44:53 +0700

    Update all.php

--- all.php
+++ all.php
@@ -19,7 +19,14 @@
 						$query=mysql_query("select * from posted where id=2");
 
 						while($data=mysql_fetch_array($query)){
-							echo '<a id="open" href="">'.$data['title'].'</a>';
+							$title=mysql_fetch_lengths($query);
+							echo '<a id="open" href="">';
+
+							for(int i=0;i<100;i++){
+								echo $title[i]; 
+							}
+
+							echo '</a>';
 							echo $data['isi'];
 						}
 					?>

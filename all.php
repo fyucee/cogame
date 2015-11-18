5f3199e2197ac182ae9090022a30050568d2b3a1
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-18 23:29:20 +0700

    Update all.php

--- all.php
+++ all.php
@@ -22,11 +22,19 @@
 							echo '<a id="open" href="">'.$data['title'].'</a>';
 							echo $data['isi'];
 						}
+					?>
+				</td>
+				<td width="376"><p align="center">
+					<?php
+						$query=mysql_query("select * from posted where id=3");
+
+						while($data=mysql_fetch_array($query)){
+							echo '<a id="open" href="">'.$data['title'].'</a>';
+							echo $data['isi'];
+						}
 
 						mysql_close();
 					?>
-				</td>
-				<td width="376">
 				</td>
 			</tr>
 			<tr align="left">

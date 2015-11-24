976bd4ba01459947ea303921b78fe31ff57e913c
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-25 02:26:57 +0700

    Update search.php

--- search.php
+++ search.php
@@ -4,6 +4,9 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
+		<?php
+			include('config.php');
+		?>
 		<table align="center" id="header" height="100" width="1166">
 			<tr>
 				<td width="5%" align="center">

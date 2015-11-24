f3e20974dcbd37757fa876a895d0c05bdff5937a
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-24 23:16:25 +0700

    Update master.php

--- admin/master.php
+++ admin/master.php
@@ -3,6 +3,26 @@
 		<link rel="stylesheet" type="text/css" href="../style.css">
 	</head>
 	<body>
-		
+		<table align="center" id="header" height="100" width="1166">
+			<tr>
+				<td width="5%" align="center">
+					<img id="title" src="../image/logo.png" width="100" height="100">
+				</td>
+				<td>
+					<a id="title" href="index.php"><font size="7">Conio Gaming ++</font></a>
+				</td>
+			</tr>
+		</table>
+		<table align="center" id="posted" width="1130">
+			<tr align="center">
+				<td height="50"><a id="open">Comment</a></td>
+				<td><a id="open">Post</a></td>
+				<td><a id="open" href="logout.php">Logout</a></td>
+				<td width="800">&nbsp;</td>
+			</tr>
+			<tr>
+				<td>&nbsp</td>
+			</tr>
+		</table>
 	</body>
 </html>

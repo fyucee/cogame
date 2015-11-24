ceebf5e98b11375816ef3237eb757f18b3efc888
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-24 22:30:43 +0700

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -3,7 +3,7 @@
 	<?php
 		session_start();
 		$_SESSION['admin']=0;
-		echo "<script>window.location='index.php?logout=success';</script>";
+		echo "<script>window.location='login.php?logout=success';</script>";
 	?>
 	</body>
 </html>

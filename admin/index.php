9979a24543145db40deafb28b579d6adcf607421
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-24 22:36:28 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,16 @@
 <html>
 	<body>
-	
+		<?php
+			if(!isset($_SESSION['admin'])){
+				include('login.php');
+			}
+			else{
+				if($_SESSION['admin']==0){
+					include('login.php');
+				}else{
+					echo 'session = '.$_SESSION['admin'];
+				}
+			}
+		?>
 	</body>
 </html>

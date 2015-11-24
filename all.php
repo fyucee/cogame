8d0aeed2f83f5487a1882419d864b6217ec1f328
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-24 19:14:29 +0700

    Update all.php

--- all.php
+++ all.php
@@ -23,28 +23,31 @@
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
+							$x+=1;
 						}
 					?>
 				</td>
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".($x+1).",1");
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
+							$x+=1;
 						}
 					?>
 				</td>
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".($x+2).",1");
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
+							$x+=1;
 						}
 					?>
 				</td>
@@ -53,34 +56,50 @@
 			<tr align="left">
 				<td><p align="center">
 					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".($x+3).",1");
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
+							$x+=1;
 						}
 					?>
 				</td>
 				<td><p align="center">
 					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".($x+4).",1");
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
+							$x+=1;
 						}
 					?>
 				</td>
 				<td><p align="center">
 					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".($x+5).",1");
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href=""><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a>';
 							echo substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="">Read more >><a/></p>';
+							$x+=1;
+						}
+					?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3"><p align="right">
+					$z=0;
+					<?php
+						if($z>0){
+							echo 'Back';
+						}
+						if($y-$x>0){
+							echo ' Next';
 						}
 					?>
 				</td>

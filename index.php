03f8ea511d3048b1bbb0b645d514dd5f2b5e13b8
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-27 03:11:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <html>
 	<head>
-		<title>Cogame</title>
+		<?php	
+			if($_GET['category']=="pc"){
+				echo'<title>Cogame: PC</title>';
+			}
+			else if($_GET['category']=="ps4"){
+				echo'<title>Cogame: PS4</title>';
+			}
+			else if($_GET['category']=="xone"){
+				echo'<title>Cogame: XBOX ONE</title>';
+			}
+			else if($_GET['category']=="wiiu"){
+				echo'<title>Cogame: Wii U</title>';
+			}
+			else if($_GET['category']=="vita"){
+				echo'<title>Cogame: PS VITA</title>';
+			}
+			else if($_GET['category']=="mobile"){
+				echo'<title>Cogame: Mobile</title>';
+			}else{
+				echo'<title>Cogame</title>';
+			}
+		?>
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>

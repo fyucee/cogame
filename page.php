44df8041c9a77cd55f7b03a92b783b717dee3076
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-27 01:24:56 +0700

    Update page.php

--- page.php
+++ page.php
@@ -10,42 +10,8 @@
 	<body>
 		<?php
 			include('config.php');
+			include('header.html');
 		?>
-		<table align="center" id="header" height="100" width="1166">
-			<tr>
-				<td width="5%" align="center">
-					<img id="title" src="image/logo.png" width="100" height="100">
-				</td>
-				<td >
-					<a id="title" href="index.php"><font size="7">Conio Gaming</font></a>
-				</td>
-			</tr>
-		</table>
-
-		<table align="center" id="menu" width="1130">
-			<tr align="center" height="50">
-				<td align="left">
-					<a href="index.php"><img id="menu" src="image/menu_all.png"></a>
-					<a href="index.php?category=pc"><img id="menu" src="image/menu_pc.png"></a>
-					<a href="index.php?category=ps4"><img id="menu" src="image/menu_ps4.png"></a>
-					<a href="index.php?category=xone"><img id="menu" src="image/menu_xboxone.png"></a>
-					<a href="index.php?category=wiiu"><img id="menu" src="image/menu_wiiu.png"></a>
-					<a href="index.php?category=vita"><img id="menu" src="image/menu_psvita.png"></a>
-					<a href="index.php?category=mobile"><img id="menu" src="image/menu_mobile.png"></a>
-				</td>
-				<td id="search" width="170">
-						<input style="border:none; width:100" type="text" placeholder="Search">
-						<input type="image" src="image/search.png" style="vertical-align: middle;" height="20px" width="20px"></a>
-				</td>
-			</tr>
-		</table>
-
-		<table  align="center" id="ads" width="1130">
-			<tr align="center" height="100">
-				<td><a href="https://play.google.com/store/apps/dev?id=6535736138875075681"><img src="image/banner.gif"></a></td>
-			</tr>
-		</table>
-		
 		<table align="center" id="posted" width="1130">
 			<tr align="center">
 				<td width="750"><p id="title" align="center">

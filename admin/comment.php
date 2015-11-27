a28870b31672794a7216f89a3b78d6c6d9708854
fyucee <yudafyuce@yahoo.co.id>
2015-11-27 18:02:06 +0700

    Comment post

---
+++ admin/comment.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../style.css">
+	</head>
+	<body>
+		<table align="center" id="header" height="100" width="1166">
+			<tr>
+				<td width="5%" align="center">
+					<img id="title" src="../image/logo.png" width="100" height="100">
+				</td>
+				<td>
+					<a id="title" href="index.php"><font size="7">Conio Gaming ++</font></a>
+				</td>
+			</tr>
+		</table>
+		<table align="center" id="posted" width="1130">
+			<tr align="center">
+				<td height="50"><a id="open">Post</a></td>
+				<td><a id="open">Comment</a></td>
+				<td><a id="open" href="logout.php">Logout</a></td>
+				<td width="800">&nbsp;</td>
+			</tr>
+			<tr>
+				<td colspan="4" align="center">
+					<?php
+						if($_GET['sub'])=="comment"){
+							include('comment.php');
+						}
+						else{
+							include('post.php');
+						}
+					?>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>

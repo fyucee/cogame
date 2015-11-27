704af7474df0d30a7c454645bdf119e54d373360
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-27 18:23:29 +0700

    Update master.php

--- admin/master.php
+++ admin/master.php
@@ -1,5 +1,8 @@
 <html>
 	<head>
+		<title>
+			Cogame ++
+		</title>
 		<link rel="stylesheet" type="text/css" href="../style.css">
 	</head>
 	<body>
@@ -12,35 +15,35 @@
 				if($_SESSION['admin']=='1'){
 					?>
 						<table align="center" id="header" height="100" width="1166">
-			<tr>
-				<td width="5%" align="center">
-					<img id="title" src="../image/logo.png" width="100" height="100">
-				</td>
-				<td>
-					<a id="title" href="index.php"><font size="7">Conio Gaming ++</font></a>
-				</td>
-			</tr>
-		</table>
-		<table align="center" id="posted" width="1130">
-			<tr align="center">
-				<td height="50"><a id="open">Post</a></td>
-				<td><a id="open">Comment</a></td>
-				<td><a id="open" href="logout.php">Logout</a></td>
-				<td width="800">&nbsp;</td>
-			</tr>
-			<tr>
-				<td colspan="4" align="center">
-					<?php
-						if($_GET['sub']=='post'){
-
-						}
-						else{
-							include('post.php');
-						}
-					?>
-				</td>
-			</tr>
-		</table>
+							<tr>
+								<td width="5%" align="center">
+									<img id="title" src="../image/logo.png" width="100" height="100">
+								</td>
+								<td>
+									<a id="title" href="index.php"><font size="7">Conio Gaming ++</font></a>
+								</td>
+							</tr>
+						</table>
+						<table align="center" id="posted" width="1130">
+							<tr align="center">
+								<td height="50"><a id="open">Post</a></td>
+								<td><a id="open">Comment</a></td>
+								<td><a id="open" href="logout.php">Logout</a></td>
+								<td width="800">&nbsp;</td>
+							</tr>
+							<tr>
+								<td colspan="4" align="center">
+									<?php
+										if($_GET['sub']=='post'){
+				
+										}
+										else{
+											include('post.php');
+										}
+									?>
+								</td>
+							</tr>
+						</table>
 					<?php
 				}else{
 					echo "<script>window.location='login.php?process=failed';</script>";

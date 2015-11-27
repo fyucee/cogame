fb5d2435f6db139a2cef279f2c0447e0e27909c1
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-27 19:51:40 +0700

    Update post.php

--- admin/post.php
+++ admin/post.php
@@ -17,13 +17,29 @@
 								<td width="200"><font size="4" color="lightblue"><b><i>Option</td>
 							</tr>
 							<tr align="center" height="30">
-								<td>id</td>
-								<td align="left">title</td>
-								<td>postdate</td>
-								<td>view</td>
-								<td><a href=""><img src="../image/view.png" title="View"></a> / 
-									<a href=""><img src="../image/edit.png" title="Edit"></a> / 
-									<a href=""><img src="../image/delete.png" title="Delete"></a></td>
+								<?php
+									if(!isset($x)){
+										$x=10;	
+									}
+									$y=0;
+
+									$query_total=mysql_query("select * from posted");
+						
+									while($data=mysql_fetch_array($query_total)){
+										$y+=1;
+									}
+
+									$query=mysql_query("select * from posted order by index desc limit ".$x.",1");
+
+									while($data=mysql_fetch_array($query)){
+										echo '<td>'.$data['id'].'</td>';
+										echo '<td align="left">'.$data['title'].'</td>';
+										echo '<td>'.$data['postdate'].'</td>';
+										echo '<td>'.$data['view'].'</td>';
+										echo '<td><a href=""><img src="../image/view.png" title="View"></a> / 
+												<a href=""><img src="../image/edit.png" title="Edit"></a> / 
+												<a href=""><img src="../image/delete.png" title="Delete"></a></td>';
+								?>
 							</tr>
 						</table>
 						<table>

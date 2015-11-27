cfe99571f3b61445efcb39b7fe3d29e3c32a9a85
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-27 19:53:11 +0700

    Update post.php

--- admin/post.php
+++ admin/post.php
@@ -18,7 +18,7 @@
 							</tr>
 							<tr align="center" height="30">
 								<?php
-									include('config.php');
+									include('../config.php');
 									if(!isset($x)){
 										$x=10;	
 									}

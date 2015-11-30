7c048406c7126b9a3462bf4fcc74437b69c2ff0d
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-12-01 01:36:50 +0700

    Update createprocess.php

--- admin/createprocess.php
+++ admin/createprocess.php
@@ -27,18 +27,11 @@
 					}
 					else if($_GET['mode']=='edit'){
 						$image=addslashes(file_get_contents($_FILES['image']['tmp_name']));						
-						$image_check=getimagesize($_FILES['image']['tmp_name']);
 
 						date_default_timezone_set('Asia/Jakarta');
 						$date = date('Y-m-d H:i:s', time());
 
-						echo $_POST['title'];
-						echo $_POST['isi'];
-						echo $_POST['cat'];
-						echo $image_check;
-						echo $image;
-
-						if(!$image_check){
+						if(!isset($image){
 							$query=mysql_query("update posted set title='".$_POST['title']."', isi='".$_POST['isi']."', cat='".$_POST['cat']."', image='$image', date='$date' where id='".$_GET['id']."'");
 						}else{
 							$query=mysql_query("update posted set title='".$_POST['title']."', isi='".$_POST['isi']."', cat='".$_POST['cat']."', date='$date' where id='".$_GET['id']."'");

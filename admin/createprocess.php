293c73ef6edeb5e52bb942368a7bec13eca4be8f
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-30 23:44:06 +0700

    Update createprocess.php

--- admin/createprocess.php
+++ admin/createprocess.php
@@ -9,17 +9,30 @@
 				if($_SESSION['admin']=='1'){
 					include("../config.php");
 
-					$image=addslashes(file_get_contents($_FILES['image']['tmp_name']));
+					if($_GET['mode']=='new'){
+						$image=addslashes(file_get_contents($_FILES['image']['tmp_name']));
 
-					date_default_timezone_set('Asia/Jakarta');
-					$date = date('Y-m-d H:i:s', time());
+						date_default_timezone_set('Asia/Jakarta');
+						$date = date('Y-m-d H:i:s', time());
 
-					$query=mysql_query("insert into posted values(null,'".$_POST['title']."','".$_POST['isi']."','".$_POST['cat']."','$image','$date','0')");
+						$query=mysql_query("insert into posted values(null,'".$_POST['title']."','".$_POST['isi']."','".$_POST['cat']."','$image','$date','0')");
 
-					if(!$query){
-						echo "<script>window.history.back();</script>";
-					}else{
-						echo "<script>window.location='index.php';</script>";
+						if(!$query){
+							echo "<script>alert('Posting Successfull');
+								window.history.back();</script>";
+						}else{
+							echo "<script>alert('Posting Failed');
+								window.location='index.php';</script>";
+						}
+					}
+					else if($_GET['mode']=='edit'){
+						if(!$query){
+							echo "<script>alert('Editing Successfull');
+								window.history.back();</script>";
+						}else{
+							echo "<script>alert('Editing Failed');
+								window.location='index.php';</script>";
+						}
 					}
 				}else{
 					echo "<script>window.location='login.php?process=failed';</script>";

035bea3c23c76a166d4a04615248b2b95b74e19c
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-30 16:51:33 +0700

    Update post.php

--- admin/post.php
+++ admin/post.php
@@ -8,7 +8,7 @@
 			else{
 				if($_SESSION['admin']=='1'){
 					?>
-						<a href="" id="title"><font size="6">+ New Post</font></a>
+						<p align="right"><a href="" id="title"><font size="5">+ New Post</font></a></p>
 						<table align="center" width="1100" border="2">
 							<tr align="center" height="50">
 								<td width="50"><font size="4"><a href="index.php"><i>Index</a></td>

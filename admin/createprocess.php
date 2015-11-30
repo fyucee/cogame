1c4b9f524d01863ea9246719ce4b056fde2f3c48
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-30 23:29:08 +0700

    Update createprocess.php

--- admin/createprocess.php
+++ admin/createprocess.php
@@ -1,4 +1,11 @@
 <html>
+	<head>
+		<title>
+			Cogame ++: New Post
+		</title>
+		<link rel='shortcut icon' type='image/x-icon' href='../image/favicon.ico'>
+		<link rel="stylesheet" type="text/css" href="../style.css">
+	</head>
 	<body>
 		<?php
 			session_start();
@@ -7,20 +14,58 @@
 			}
 			else{
 				if($_SESSION['admin']=='1'){
-					include("../config.php");
-
-					$image=addslashes(file_get_contents($_FILES['image']['tmp_name']));
-
-					date_default_timezone_set('Asia/Jakarta');
-					$date = date('Y-m-d H:i:s', time());
-
-					$query=mysql_query("insert into posted values(null,'".$_POST['title']."','".$_POST['isi']."','".$_POST['cat']."','$image','$date','0')");
-
-					if(!$query){
-						echo "<script>window.history.back();</script>";
-					}else{
-						echo "<script>window.location='index.php';</script>";
-					}
+					?>
+						<table align="center" id="header" height="100" width="1166">
+							<tr>
+								<td width="5%" align="center">
+									<img id="title" src="../image/logo.png" width="100" height="100">
+								</td>
+								<td>
+									<a id="title" href="index.php"><font size="7">Conio Gaming ++</font></a>
+								</td>
+							</tr>
+						</table>
+						<table align="center" id="posted" width="1130">
+							<form action="createprocess.php?mode=".$_GET['mode']."" method="post" enctype="multipart/form-data">
+								<tr><td height="15"></td></tr>
+								<tr align="center">
+								<?php
+									if(!isset($_GET['mode'])){
+										echo '<td colspan="3"><font size="6"><b>New Post</b></font></td>';
+									}else{
+										echo '<td colspan="3"><font size="6"><b>Edit Post</b></font></td>';
+									}
+								?>
+								</tr>
+								<tr>
+									<td width="110" rowspan="5"></td>
+									<td height="50">&nbsp;<font size="5"><b>Title</b></font></td>
+									<td><input name="title" type="text" style="width:800"></td>
+								</tr>
+								<tr>
+									<td>&nbsp;<font size="5"><b>Content</b></font></td>
+									<td><textarea name="isi"></textarea></td>
+								</tr>
+								<tr>
+									<td>&nbsp;<font size="5"><b>Category</b></font></td>
+									<td><input name="cat" type="text" style="width:800"></input></td>
+								</tr>
+								<tr>
+									<td></td>
+									<td><font size="2"><b>&nbsp;Note: If more than one, separate that by space.</b></font></td>
+								</tr>
+								<tr>
+									<td height="50">&nbsp;<font size="5"><b>Image</b></font></td>
+									<td><input name="image" type="file"></input></td>
+								</tr>
+								<tr>
+									<td align="center" height="50" colspan="4">
+										<input type="submit" value="Submit"></input>
+									</td>
+								</tr>
+							</form>
+						</table>
+						<?php
 				}else{
 					echo "<script>window.location='login.php?process=failed';</script>";
 				}

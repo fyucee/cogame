691880bf5dd96577693163bc1d9b188b0e4aff96
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-30 16:07:36 +0700

    Update category.php

--- category.php
+++ category.php
@@ -6,30 +6,23 @@
 			<tr align="left">
 				<td width="376"><p align="center">
 					<?php
-						if(!isset($x)){
-							$x=0;	
-						}
-						$y=0;
-
 						$query_total=mysql_query("select * from posted where cat like '%".$_GET['category']."%'");
 						
-						while($data=mysql_fetch_array($query_total)){
-							$y+=1;
-						}
+						$row=mysql_num_rows($query_total);
+						$pageTotal=ceil($row/6);
 
-						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".$x.",1");
-
-						while($data=mysql_fetch_array($query)){
-							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
-							echo '<a href="page.php?title='.$data['title'].'&index='.$data['id'].'"><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
-							echo '<p align="justify">'.substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="page.php?title='.$data['title'].'&index='.$data['id'].'">Read more >><a/></p>';
-							$x+=1;
+						if(!isset($_GET['page'])){
+							$page=1;	
+						}else if($_GET['page']<1){
+							$page=1;
+						}else if ($_GET['page']>$pageTotal){
+							$page=$pageTotal;
+						}else{
+							$page=$_GET['page'];
 						}
-					?>
-				</td>
-				<td width="376"><p align="center">
-					<?php
-						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".$x.",1");
+						$x=$page*6;
+
+						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".($x-6).",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
@@ -41,7 +34,19 @@
 				</td>
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".$x.",1");
+						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".($x-5).",1");
+
+						while($data=mysql_fetch_array($query)){
+							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
+							echo '<a href="page.php?title='.$data['title'].'&index='.$data['id'].'"><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
+							echo '<p align="justify">'.substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="page.php?title='.$data['title'].'&index='.$data['id'].'">Read more >><a/></p>';
+							$x+=1;
+						}
+					?>
+				</td>
+				<td width="376"><p align="center">
+					<?php
+						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".($x-4).",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
@@ -60,7 +65,7 @@
 			<tr align="left">
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".$x.",1");
+						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".($x-3).",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
@@ -72,7 +77,7 @@
 				</td>
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".$x.",1");
+						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".($x-2).",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
@@ -84,7 +89,7 @@
 				</td>
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".$x.",1");
+						$query=mysql_query("select * from posted where cat like '%".$_GET['category']."%' order by postdate desc limit ".($x-1).",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
@@ -95,14 +100,18 @@
 				</td>
 			</tr>
 			<tr>
-				<td colspan="3"><p align="right">
+				<td><p align="left">
 					<?php
-						$z=0;
-						if($z>0){
-							echo '<a href="">Back</a>';
+						if($page>1){
+							echo "<a id='title' href='index.php?category=".$_GET['category']."&page=".($page-1)."'><font size='7'>Back</font></a>";
 						}
-						if($y-$x>1){
-							echo ' <a href="">Next</a>';
+					?>
+				</td>
+				<td></td>
+				<td><p align="right">
+					<?php
+						if($page<$pageTotal){
+							echo "<a id='title' href='index.php?category=".$_GET['category']."&page=".($page+1)."'><font size='7'>Next</font></a>";
 						}
 					?>
 				</td>

233cc43d1a982f958cd418a27a3403e73c241648
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-30 15:04:11 +0700

    Update all.php

--- all.php
+++ all.php
@@ -6,48 +6,50 @@
 			<tr align="left">
 				<td width="376"><p align="center">
 					<?php
-						if(!isset($x)){
-							$x=0;	
-						}
-						$y=0;
-
 						$query_total=mysql_query("select * from posted");
 						
-						while($data=mysql_fetch_array($query_total)){
-							$y+=1;
-						}
+						$row=mysql_num_rows($query_total);
+						$pageTotal=ceil($row/6);
 
-						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
+						if(!isset($_GET['page'])){
+							$page=1;	
+						}else if($_GET['page']<1){
+							$page=1;
+						}else if ($_GET['page']>$pageTotal){
+							$page=$pageTotal;
+						}else{
+							$page=$GET['page'];
+						}
+						$x=$page*6;
+
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x-6.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href="page.php?title='.$data['title'].'&index='.$data['id'].'"><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<p align="justify">'.substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="page.php?title='.$data['title'].'&index='.$data['id'].'">Read more >><a/></p>';
-							$x+=1;
-						}
-					?>
-				</td>
-				<td width="376"><p align="center">
-					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
-
-						while($data=mysql_fetch_array($query)){
-							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
-							echo '<a href="page.php?title='.$data['title'].'&index='.$data['id'].'"><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
-							echo '<p align="justify">'.substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="page.php?title='.$data['title'].'&index='.$data['id'].'">Read more >><a/></p>';
-							$x+=1;
 						}
 					?>
 				</td>
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x-5.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href="page.php?title='.$data['title'].'&index='.$data['id'].'"><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<p align="justify">'.substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="page.php?title='.$data['title'].'&index='.$data['id'].'">Read more >><a/></p>';
-							$x+=1;
+						}
+					?>
+				</td>
+				<td width="376"><p align="center">
+					<?php
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x-4.",1");
+
+						while($data=mysql_fetch_array($query)){
+							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
+							echo '<a href="page.php?title='.$data['title'].'&index='.$data['id'].'"><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
+							echo '<p align="justify">'.substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="page.php?title='.$data['title'].'&index='.$data['id'].'">Read more >><a/></p>';
 						}
 					?>
 				</td>
@@ -60,31 +62,29 @@
 			<tr align="left">
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x-3.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href="page.php?title='.$data['title'].'&index='.$data['id'].'"><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<p align="justify">'.substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="page.php?title='.$data['title'].'&index='.$data['id'].'">Read more >><a/></p>';
-							$x+=1;
 						}
 					?>
 				</td>
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x-2.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
 							echo '<a href="page.php?title='.$data['title'].'&index='.$data['id'].'"><img id="posted" src="data:image/jpeg;base64,'.base64_encode( $data['image'] ).'"/></a><br>';
 							echo '<p align="justify">'.substr($data['isi'], 0,300).' ...</p><p align="right"><a id="next" href="page.php?title='.$data['title'].'&index='.$data['id'].'">Read more >><a/></p>';
-							$x+=1;
 						}
 					?>
 				</td>
 				<td width="376"><p align="center">
 					<?php
-						$query=mysql_query("select * from posted order by postdate desc limit ".$x.",1");
+						$query=mysql_query("select * from posted order by postdate desc limit ".$x-1.",1");
 
 						while($data=mysql_fetch_array($query)){
 							echo '<a id="open" href="page.php?title='.$data['title'].'&index='.$data['id'].'">'.substr($data['title'],0,60).' ...</a>';
@@ -97,12 +97,11 @@
 			<tr>
 				<td colspan="3"><p align="right">
 					<?php
-						$z=0;
-						if($z>0){
-							echo '<a href="">Back</a>';
+						if($page>1){
+							echo "<a href='index.php?page=".($page-1)."'>Back</a>";
 						}
-						if($y-$x>1){
-							echo ' <a href="">Next</a>';
+						if($page<$pageTotal){
+							echo "<a href='index.php?page=".($page+1)."'>Next</a>";
 						}
 					?>
 				</td>

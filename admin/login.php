727fcc4ed68a0a1cb5cedfa958677bdccffd2d87
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-12-01 01:58:39 +0700

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,102 +1,69 @@
 <html>
 	<head>
-		<title>Loading...</title>
+		<link rel="stylesheet" type="text/css" href="../style.css">
+		<title>
+			Cogame ++
+		</title>
 	</head>
 	<body>
+		<table align="center" height="100">
+			<tr>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+		<table align="center" height="200">
+			<tr>
+				<td align="center"><img id="title" src="../image/logo.png" width="100" height="100"><br>
+					<a id="title" href="../index.php"><font color="black" size="7"><b>Conio Gaming</font></a>
+					<br><font color="black" size="5">Admin
+				</td>
+			</tr>
+		</table>
+		<table align="center" id="posted" width="300">
+			<form action="process.php" method="post">
+				<tr align="center" height="50">
+					<td>Username</td>
+					<td>:</td>
+					<td><input name="user" type="text"></td>
+				</tr>
+				<tr align="center" height="50">
+					<td>Password</td>
+					<td>:</td>
+					<td><input name="pw" type="password"></td>
+				</tr>
+				<tr align="center" height="50">
+					<td colspan="3"><input type="submit" value="Login"></td>
+				</tr>
+			</form>
+		</table>
 		<?php
-			session_start();
-			if(!isset($_SESSION['admin'])){
-				echo "<script>window.location='login.php?process=failed';</script>";
+			if($_GET['login']=='failed'){
+		?>
+			<table bgcolor="red" align="center" width="300">
+				<tr align="center">
+					<td>Invalid Username or Password</td>
+				</tr>
+			</table>
+		<?php
 			}
-			else{
-				if($_SESSION['admin']=='1'){
-					include("../config.php");
-
-					if($_GET['mode']=='new'){
-						$image=addslashes(file_get_contents($_FILES['image']['tmp_name']));
-
-						date_default_timezone_set('Asia/Jakarta');
-						$date = date('Y-m-d H:i:s', time());
-
-						$query=mysql_query("insert into posted values(null,'".$_POST['title']."','".$_POST['isi']."','".$_POST['cat']."','$image','$date','0')");
-
-						if(!$query){
-							echo "<script>alert('Posting Failed');
-								window.history.back();</script>";
-						}else{
-							echo "<script>alert('Posting Successfull');
-								window.location='index.php';</script>";
-						}
-					}
-					else if($_GET['mode']=='edit'){
-						$image=addslashes(file_get_contents($_FILES['image']['tmp_name']));						
-						$image_check=getimagesize($_FILES['image']['tmp_name']);
-
-						date_default_timezone_set('Asia/Jakarta');
-						$date = date('Y-m-d H:i:s', time());
-
-						if(!$image_check){
-							$query=mysql_query("update posted set title='".$_POST['title']."', isi='".$_POST['isi']."', cat='".$_POST['cat']."', postdate='$date' where id='".$_POST['id']."'");
-						}else{
-							$query=mysql_query("update posted set title='".$_POST['title']."', isi='".$_POST['isi']."', cat='".$_POST['cat']."', image='$image', postdate='$date' where id='".$_POST['id']."'");
-						}
-
-						if(!$query){
-							echo "<script>alert('Editing Failed');
-								window.history.back();</script>";
-						}else{
-							echo "<script>alert('Editing Successfull');
-								window.location='index.php';</script>";
-						}
-					}
-				}else{
-					echo "<script>window.location='login.php?process=failed';</script>";
-				}
+			if($_GET['logout']=='success'){
+		?>
+			<table bgcolor="green" align="center" width="300">
+				<tr align="center">
+					<td>You've been logout</td>
+				</tr>
+			</table>
+		<?php
+			}
+			if($_GET['process']=='failed'){
+		?>
+			<table bgcolor="red" align="center" width="300">
+				<tr align="center">
+					<td>You must login first!</td>
+				</tr>
+			</table>
+		<?php
 			}
 		?>
-	</body>
-</html>
-
-	<body>
-		<?php
-			session_start();
-			if(!isset($_SESSION['admin'])){
-				echo "<script>window.location='login.php?process=failed';</script>";
-			}
-			else{
-				if($_SESSION['admin']=='1'){
-					include("../config.php");
-
-					$query=mysql_query("delete from posted where id = '".$_GET['id']."'");
-
-					echo "<script>alert('Delete Successfull');
-								window.location='index.php';</script>";
-				}else{
-					echo "<script>window.location='login.php?process=failed';</script>";
-				}
-			}
-		?>
-	</body>
-</html>
-
-	<body>
-	<?php
-		session_start();
-		include("../config.php");
-	
-		$query=mysql_query("select * from admin
-						where user = '".$_POST['user']."'
-						and pw = '".$_POST['pw']."'");
-
-		$check=mysql_num_rows($query);
-		
-		if($check > 0){
-			$_SESSION['admin']=1;
-			echo "<script>window.location='index.php';</script>";
-		}
-		else{
-			echo "<script>window.location='login.php?login=failed';</script>";
-		}
-	?>
 	</body>
 </html>

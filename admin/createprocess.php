123d3f5b43ba135b6b08df9c25baf84c4d30a482
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-11-30 22:44:18 +0700

    Create createprocess.php

---
+++ admin/createprocess.php
@@ -0,0 +1,26 @@
+<html>
+	<body>
+		<?php
+			session_start();
+			if(!isset($_SESSION['admin'])){
+				echo "<script>window.location='login.php?process=failed';</script>";
+			}
+			else{
+				if($_SESSION['admin']=='1'){
+					include("../config.php");
+
+					$image=addslashes(file_get_contents($_FILES['image']['tmp_name']));
+
+					date_default_timezone_set('Asia/Jakarta');
+					$date = date('Y-m-d h:i:s', time());
+
+					$query=mysql_query("insert into posted values('','".$_POST['title']."','".$_POST['isi']."','".$_POST['cat']."','$image','$date',0)");
+
+					echo "<script>window.location='index.php';</script>";
+				}else{
+					echo "<script>window.location='login.php?process=failed';</script>";
+				}
+			}
+		?>
+	</body>
+</html>

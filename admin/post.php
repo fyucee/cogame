eecfeef47a055ac1cfd6e5a65db70e8f0f285dd3
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-12-01 15:23:07 +0700

    Update post.php

--- admin/post.php
+++ admin/post.php
@@ -76,7 +76,7 @@
 									while($data=mysql_fetch_array($query)){
 										echo '<tr align="center" height="30">';
 										echo '<td>'.$data['id'].'</td>';
-										echo '<td align="left"><a id="title" href=""><u>'.$data['title'].'</u></a></td>';
+										echo '<td align="left"><a id="title" href="">'.$data['title'].'</a></td>';
 										echo '<td>'.$data['postdate'].'</td>';
 										echo '<td>'.$data['view'].'</td>';
 										echo '<td><a href="../page.php?title='.$data['title'].'&index='.$data['id'].'"><img src="../image/view.png" title="View"></a> / 

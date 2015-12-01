6f7e88fe47a3212068771d93e266005a51e3934f
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-12-01 13:01:31 +0700

    Update page.php

--- page.php
+++ page.php
@@ -141,7 +141,7 @@
 
 				while($data=mysql_fetch_array($query)){
 					echo'<tr>
-						<td colspan="2"><font size="4">&nbsp;'.$data['name'].'</font>
+						<td colspan="2"><font size="4">&nbsp;'.$data['name'].', said:</font>
 						<p align="justify" width="750"><font size="3">&nbsp;'.$data['comment'].'</font></p></td>
 						<td colspan="2"></td>
 					</tr>';

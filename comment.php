704d87b0e4ebbe368417ef82a51e9c718681abbd
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-12-01 12:36:36 +0700

    Update comment.php

--- comment.php
+++ comment.php
@@ -8,9 +8,16 @@
 
 			date_default_timezone_set('Asia/Jakarta');
 			$date = date('Y-m-d H:i:s', time());
-			
+
 			$query=mysql_query("insert into comment values('".$_POST['id']."',null,'".$_POST['name']."','".$_POST['mail']."','".$_POST['comment']."','$date')");
 
+			if(!$query){
+				echo "<script>alert('Submitting Failed');
+					window.history.back();</script>";
+			}else{
+				echo "<script>alert('Submitting Successfull');
+					window.location='page.php?title=".$_POST['title']."&index=".$_POST['id']."';</script>";
+			}
 		?>
 	</body>
 </html>

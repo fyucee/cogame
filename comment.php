35620d212e9a4d1e4867eeff58d151b2d2649c71
Wahyu Hidayat <yudafyuce@yahoo.co.id>
2015-12-01 12:32:02 +0700

    Update comment.php

--- comment.php
+++ comment.php
@@ -8,13 +8,8 @@
 
 			date_default_timezone_set('Asia/Jakarta');
 			$date = date('Y-m-d H:i:s', time());
-
-      echo $_POST['id'];
-      echo $_POST['name'];
-      echo $_POST['mail'];
-      echo $_POST['comment'];
-      
-			//$query=mysql_query("insert into comment values('".$_POST['id']."',null,'".$_POST['name']."','".$_POST['mail']."','".$_POST['comment']."','$date')");
+			
+			$query=mysql_query("insert into comment values('".$_POST['id']."',null,'".$_POST['name']."','".$_POST['mail']."','".$_POST['comment']."','$date')");
 
 		?>
 	</body>
